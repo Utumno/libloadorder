e3dfa5bf7cc6c5c7791d941c1b772c0733f6320e
Oliver Hamlet <oliver.hamlet@gmail.com>
2014-10-05 12:56:16 +0100

    [TESTS-TRAVIS-CMAKE]Restructured test fixtures.
    	Plugins are now stored in a separate repository, and have descriptive
    	names. Moved almost all fixture assertions into fixture SetUp/TearDown.
    	Reduced code duplication by inheriting from another class. Moved most
    	testing environment setup into fixture SetUp, so that tests share a more
    	common environment.
    
    	A few tests currently fail, it looks like there are issues with setting
    	load order.
    
    	# This is the 2nd commit message:
    
    	Made initial load order / active state consistent.
    
    	It turns out that was the reason for the failed tests in the previous
    	commit.
    
    	# This is the 3rd commit message:
    
    	Fixed GCC linking error.
    
    	There's also an exception being thrown somewhere in
    	SkyrimOperationsTest::SetUp, hence the ASSERT_NO_THROW() additions to
    	help track it down.

--- src/tests/fixtures.h
+++ src/tests/fixtures.h
@@ -27,7 +27,16 @@ along with libloadorder.  If not, see
 #define __LIBLO_TEST_FIXTURES__
 
 #include "../api/libloadorder.h"
+#include "backend/streams.h"
+
 #include <gtest/gtest.h>
+
+#ifdef __GNUC__  // Workaround for GCC linking error.
+#pragma message("GCC detected: Defining BOOST_NO_CXX11_SCOPED_ENUMS to avoid linking errors for boost::filesystem::copy_file().")
+#define BOOST_NO_CXX11_SCOPED_ENUMS
+#define BOOST_NO_SCOPED_ENUMS  // For older versions.
+#endif
+#include <boost/filesystem.hpp>
 
 class GameHandleCreationTest : public ::testing::Test {
 protected:
@@ -40,30 +49,165 @@ protected:
     lo_game_handle gh;
 };
 
-class OblivionOperationsTest : public ::testing::Test {
+class GameOperationsTest : public ::testing::Test {
 protected:
+    GameOperationsTest(const boost::filesystem::path& gameDataPath, const boost::filesystem::path& gameLocalPath)
+        : dataPath(gameDataPath), localPath(gameLocalPath), gh(NULL) {}
+
     inline virtual void SetUp() {
-        ASSERT_EQ(LIBLO_OK, lo_create_handle(&gh, LIBLO_GAME_TES4, "./game", "./local"));
+        ASSERT_NO_THROW(boost::filesystem::create_directories(localPath));
+        ASSERT_TRUE(boost::filesystem::exists(localPath));
+
+        ASSERT_TRUE(boost::filesystem::exists(dataPath / "Blank.esm"));
+        ASSERT_TRUE(boost::filesystem::exists(dataPath / "Blank - Different.esm"));
+        ASSERT_TRUE(boost::filesystem::exists(dataPath / "Blank - Master Dependent.esm"));
+        ASSERT_TRUE(boost::filesystem::exists(dataPath / "Blank - Different Master Dependent.esm"));
+        ASSERT_TRUE(boost::filesystem::exists(dataPath / "Blank.esp"));
+        ASSERT_TRUE(boost::filesystem::exists(dataPath / "Blank - Different.esp"));
+        ASSERT_TRUE(boost::filesystem::exists(dataPath / "Blank - Master Dependent.esp"));
+        ASSERT_TRUE(boost::filesystem::exists(dataPath / "Blank - Different Master Dependent.esp"));
+        ASSERT_TRUE(boost::filesystem::exists(dataPath / "Blank - Plugin Dependent.esp"));
+        ASSERT_TRUE(boost::filesystem::exists(dataPath / "Blank - Different Plugin Dependent.esp"));
+
+        ASSERT_FALSE(boost::filesystem::exists(dataPath / "Blank.esm.missing"));
+        ASSERT_FALSE(boost::filesystem::exists(dataPath / "Blank.esp.missing"));
+
+        // Ghost a plugin.
+        ASSERT_FALSE(boost::filesystem::exists(dataPath / "Blank - Master Dependent.esm.ghost"));
+        ASSERT_NO_THROW(boost::filesystem::rename(dataPath / "Blank - Master Dependent.esm", dataPath / "Blank - Master Dependent.esm.ghost"));
+        ASSERT_TRUE(boost::filesystem::exists(dataPath / "Blank - Master Dependent.esm.ghost"));
+
+        // Write out an empty file.
+        liblo::ofstream out(dataPath / "EmptyFile.esm");
+        out.close();
+        ASSERT_TRUE(boost::filesystem::exists(dataPath / "EmptyFile.esm"));
+
+        // Write out an non-empty, non-plugin file.
+        out.open(dataPath / "NotAPlugin.esm");
+        out << "This isn't a valid plugin file.";
+        out.close();
+        ASSERT_TRUE(boost::filesystem::exists(dataPath / "NotAPlugin.esm"));
     }
 
     inline virtual void TearDown() {
-        lo_destroy_handle(gh);
-    };
+        // Unghost the ghosted plugin.
+        ASSERT_TRUE(boost::filesystem::exists(dataPath / "Blank - Master Dependent.esm.ghost"));
+        ASSERT_NO_THROW(boost::filesystem::rename(dataPath / "Blank - Master Dependent.esm.ghost", dataPath / "Blank - Master Dependent.esm"));
+        ASSERT_FALSE(boost::filesystem::exists(dataPath / "Blank - Master Dependent.esm.ghost"));
+
+        // Delete generated files.
+        ASSERT_NO_THROW(boost::filesystem::remove(dataPath / "EmptyFile.esm"));
+        ASSERT_NO_THROW(boost::filesystem::remove(dataPath / "NotAPlugin.esm"));
+        ASSERT_FALSE(boost::filesystem::exists(dataPath / "EmptyFile.esm"));
+        ASSERT_FALSE(boost::filesystem::exists(dataPath / "NotAPlugin.esm"));
+
+        ASSERT_NO_THROW(lo_destroy_handle(gh));
+    }
+
+    const boost::filesystem::path dataPath;
+    const boost::filesystem::path localPath;
 
     lo_game_handle gh;
 };
 
-class SkyrimOperationsTest : public ::testing::Test {
+class OblivionOperationsTest : public GameOperationsTest {
 protected:
+    OblivionOperationsTest() : GameOperationsTest("./Oblivion/Data", "./local/Oblivion") {}
+
     inline virtual void SetUp() {
-        ASSERT_EQ(LIBLO_OK, lo_create_handle(&gh, LIBLO_GAME_TES5, "./game", "./local"));
+        GameOperationsTest::SetUp();
+
+        // Oblivion's load order is decided through timestamps, so reset them to a known order before each test.
+        std::list<std::string> loadOrder = {
+            "Blank.esm",
+            "Blank - Different.esm",
+            "Blank - Master Dependent.esm",  // Ghosted
+            "Blank - Different Master Dependent.esm",
+            "Blank.esp",
+            "Blank - Different.esp",
+            "Blank - Master Dependent.esp",
+            "Blank - Different Master Dependent.esp",
+            "Blank - Plugin Dependent.esp",
+            "Blank - Different Plugin Dependent.esp"
+        };
+        time_t modificationTime = time(NULL);  // Current time.
+        for (const auto &plugin : loadOrder) {
+            if (boost::filesystem::exists(dataPath / boost::filesystem::path(plugin + ".ghost"))) {
+                boost::filesystem::last_write_time(dataPath / boost::filesystem::path(plugin + ".ghost"), modificationTime);
+            }
+            else {
+                boost::filesystem::last_write_time(dataPath / plugin, modificationTime);
+            }
+            modificationTime += 60;
+        }
+
+        // Set Oblivion's active plugins to a known list before running the test.
+        liblo::ofstream activePlugins(localPath / "plugins.txt");
+        activePlugins
+            << "Blank.esm" << std::endl;
+        activePlugins.close();
+
+        ASSERT_EQ(LIBLO_OK, lo_create_handle(&gh, LIBLO_GAME_TES4, dataPath.parent_path().string().c_str(), localPath.string().c_str()));
     }
 
     inline virtual void TearDown() {
-        lo_destroy_handle(gh);
-    };
+        GameOperationsTest::TearDown();
 
-    lo_game_handle gh;
+        // Delete existing plugins.txt.
+        ASSERT_NO_THROW(boost::filesystem::remove(localPath / "plugins.txt"));
+    };
+};
+
+class SkyrimOperationsTest : public GameOperationsTest {
+protected:
+    SkyrimOperationsTest() : GameOperationsTest("./Skyrim/Data", "./local/Skyrim") {}
+
+    inline virtual void SetUp() {
+        GameOperationsTest::SetUp();
+
+        // Can't change Skyrim's main master file, so mock it.
+        ASSERT_FALSE(boost::filesystem::exists(dataPath / "Skyrim.esm"));
+        ASSERT_NO_THROW(boost::filesystem::copy_file(dataPath / "Blank.esm", dataPath / "Skyrim.esm"));
+        ASSERT_TRUE(boost::filesystem::exists(dataPath / "Skyrim.esm"));
+
+        // Set Skyrim's load order to a known list before running the test.
+        liblo::ofstream loadOrder(localPath / "loadorder.txt");
+        loadOrder
+            << "Skyrim.esm" << std::endl
+            << "Blank.esm" << std::endl
+            << "Blank - Different.esm" << std::endl
+            << "Blank - Master Dependent.esm" << std::endl  // Ghosted
+            << "Blank - Different Master Dependent.esm" << std::endl
+            << "Blank.esp" << std::endl
+            << "Blank - Different.esp" << std::endl
+            << "Blank - Master Dependent.esp" << std::endl
+            << "Blank - Different Master Dependent.esp" << std::endl
+            << "Blank - Plugin Dependent.esp" << std::endl
+            << "Blank - Different Plugin Dependent.esp" << std::endl;
+        loadOrder.close();
+
+        // Set Skyrim's active plugins to a known list before running the test.
+        liblo::ofstream activePlugins(localPath / "plugins.txt");
+        activePlugins
+            << "Skyrim.esm" << std::endl
+            << "Blank.esm" << std::endl;
+        activePlugins.close();
+
+        ASSERT_EQ(LIBLO_OK, lo_create_handle(&gh, LIBLO_GAME_TES5, dataPath.parent_path().string().c_str(), localPath.string().c_str()));
+    }
+
+    inline virtual void TearDown() {
+        GameOperationsTest::TearDown();
+
+        // Delete the mock Skyrim.esm.
+        ASSERT_TRUE(boost::filesystem::exists(dataPath / "Skyrim.esm"));
+        ASSERT_NO_THROW(boost::filesystem::remove(dataPath / "Skyrim.esm"));
+        ASSERT_FALSE(boost::filesystem::exists(dataPath / "Skyrim.esm"));
+
+        // Delete existing plugins.txt and loadorder.txt.
+        ASSERT_NO_THROW(boost::filesystem::remove(localPath / "plugins.txt"));
+        ASSERT_NO_THROW(boost::filesystem::remove(localPath / "loadorder.txt"));
+    };
 };
 
 #endif
